215a5308ab779e7127e41d47288ffcd4893d9e40
Matt Brigida <mdbrigida@gmail.com>
2020-03-06 20:54:23 -0500

    some packages

--- Project.py
+++ Project.py
@@ -2,3 +2,7 @@
 
 ## Created by Katelin Barrera
 ## Created on March 4 2020
+
+### We'll need to install a few python libraries.  I would recommend using pip. 
+### Libraries: numpy, pandas, scipy
+### You may also want to install tensorflow or pytorch to try some ML methods.
